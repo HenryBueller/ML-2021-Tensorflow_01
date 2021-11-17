9e4164861163fef89d47bb36e16a3398bcdf5f33
Henry Bueller <johndoe@doebrothers.com>
2021-11-17 18:18:59 +0800

    chap2

---
+++ chap2/exp_2_2_mnist_mlp_dlp_4Layers/stu_upload/__init__.py
@@ -0,0 +1,3 @@
+# bzpskdwyfjqb
+
+# scp -P 27030 -r C:/Users/A/Desktop/code/exp_2_2_mnist_mlp_dlp_4Layers/stu_upload root@120.236.247.203:/opt/code_chap_2_3/code_chap_2_3_student/exp_2_2_mnist_mlp_dlp
